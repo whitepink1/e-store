ba589b894f5ebe61ffb0840f85573641b2129d8f
kiruhat <kiruhat9@gmail.com>
2026-06-03 18:12:57 +0300

    Checkout form upgrade.

--- apps/web/components/Products/CheckoutForm.tsx
+++ apps/web/components/Products/CheckoutForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+'use client'
 import { Product } from '../../lib/validations/product'
 import { CartItem } from '../../lib/validations/user';
 import { useForm } from 'react-hook-form';
@@ -8,13 +8,36 @@ import { zodResolver } from '@hookform/resolvers/zod';
 interface CheckoutFormProps {
     products: Product[];
     orderItems: CartItem[];
-}
-
-type CheckoutFormValues = Omit<Order, 'userId' | 'products' | 'totalPrice' | 'createdAt' | 'updatedAt'>;
+};
 
 const CheckoutForm = ({products, orderItems}: CheckoutFormProps) => {
-    const {register, handleSubmit, formState: {errors}} = useForm<CheckoutFormValues>({
-        resolver: zodResolver(OrderSchema)
+    const orderProducts = orderItems.map(item => {
+        const sameProduct = products.find(product => product._id === item.productId);
+        if (sameProduct) {
+            return {...item, price: sameProduct.variants[Number(item.variantId)]?.finalPrice}
+        }
+    });
+    const totalPrice = orderProducts.reduce((sum, product) => {
+        return sum + (Number(product?.quantity) * Number(product?.price))
+    }, 79);
+    console.log(totalPrice)
+    const {register, handleSubmit, formState: {errors}} = useForm<Order>({
+        resolver: zodResolver(OrderSchema),
+        defaultValues: {
+            userId: '',
+            products: orderProducts || [],
+            address: {
+                country: "",
+                city: "",
+                street: "",
+                apartment: "",
+                postalCode: "",
+                phone: "",
+            },
+            shipment: '',
+            totalPrice: totalPrice || 0,
+            status: 'Pending',
+        },
     });
 
     const handleSubmitOrder = async (e: React.FormEvent) => {
