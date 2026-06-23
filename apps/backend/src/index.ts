a4f923c81ef74df72edd39db774459cf783d0456
kiruhat <kiruhat9@gmail.com>
2026-06-23 15:53:02 +0300

    Updated cors

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -15,7 +15,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(helmet());
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ 
+    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    credentials: true
+}));
 app.use(morgan('dev'));
 app.use(express.json());
 
